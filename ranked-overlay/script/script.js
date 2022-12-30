5bebe2cc17213fffe9c5fd9af4c4c0d8c5d90a24
Oolonk <dortimus51@gmail.com>
2022-12-30 17:09:51 +0100

    update slippi script

--- ranked-overlay/script/script.js
+++ ranked-overlay/script/script.js
@@ -34,15 +34,48 @@ const query = `fragment userProfilePage on User {
             }
     }`;
 
-const req = fetch('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
-headers: {
-    'content-type': 'application/json',
-},
-body: JSON.stringify({
-    operationName: 'AccountManagementPageQuery',
-    query,
-    variables: { cc: connectCode },
-}),
-method: 'POST',
-});
+const req = 
 console.log( req);
+async function req(){
+    let result;
+    await fetch('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            operationName: 'AccountManagementPageQuery',
+            query,
+            variables: { cc: connectCode },
+        }),
+        method: 'POST',
+        })
+        .then((res) => {result = JSON.parse(res)})
+        return(result);
+}
+function getRank(rating, games, globalPlacement, regionalPlacement){
+    if(games < 5){
+        return 'Unranked';
+    }else if(rating >= 2191.75 && globalPlacement != null && regionalPlacement != null){
+        return 'Grandmaster';
+    }else if(rating >= 2350){
+        return 'Master 3';
+    }else if(rating >= 2275){
+        return 'Master 2';
+    }else if(rating >= 2191.75){
+        return 'Master 1';
+    }else if(rating >= 2136.28){
+        return 'Diamond 3';
+    }else if(rating >= 2073.67){
+        return 'Diamond 2';
+    }else if(rating >= 2003.92){
+        return 'Diamond 1';
+    }else if(rating >= 1927.03){
+        return 'Platinum 3';
+    }else if(rating >= 1843){
+        return 'Platinum 2';
+    }else if(rating >= 1751.83){
+        return 'Platinum 1';
+    }else if(rating >= 1653.52){
+        return 'Gold 3';
+    }
+}
